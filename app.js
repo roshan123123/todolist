42db42619317555710051b05cb04e40a75b8f190
Roshan <kumarroshan7461@gmail.com>
2021-04-24 23:02:01 +0530

    added .env file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // incorporating todo list on local server using mongodb database using ongoose
 
-
+require('dotenv').config();
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));//to use body parser
 const date=require(__dirname+"/module.js");//this will go in that module and run all the code inside it
 app.set('view engine', 'ejs');//for ejs to work
 app.use(express.static("public"));//this will let other static files like css and images all to render
-mongoose.connect("mongodb+srv://Roshan:Roshan@cluster0.xtrcp.mongodb.net/toDoDB") ;//toDoDB is the name of database copied url neesd some changes pass and dbnameafternettill end
+mongoose.connect(process.env.ram); //toDoDB is the name of database copied url neesd some changes pass and dbnameafternettill end
 // mongoose.connect("mongodb://localhost:27017/toDoDB") ;//toDoDB is the name of database when connecting locally
 const _=require("lodash");
 //craeting a schema
